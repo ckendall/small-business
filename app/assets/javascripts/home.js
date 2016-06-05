371ee7e7f0fd53e0cb6552ba647767e66c11d401
Cody <cody.kendall21@gmail.com>
2016-06-05 11:41:27 -0700

    coming along

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,3 +1,7 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map-canvas'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
